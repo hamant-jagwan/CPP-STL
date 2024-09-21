0a59f21ffef1cf8d70c66ff3b277faa4d50e025e
Hamant Jagwan <Hemant7jagwan@gmail.com>
2024-09-21 23:59:33 +0530

    update

--- OOPs/Inherit2.cpp
+++ OOPs/Inherit2.cpp
@@ -15,8 +15,19 @@ class derived : public base{
     //Empty
 };
 
+class derived2 : public base{
+   public:
+    derived2(){
+        cout << "Default Constructor of Derived Class";
+    }
+    derived2(int d){
+        cout << "Para Constructor of Derived Class" << d <<endl;
+    }
+};
+
+
 int main(){
-    derived d;
+    derived2 d;
     // derived d(23) Only take default constructor
     return 0;
 }
